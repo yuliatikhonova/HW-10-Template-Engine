557193b1281de75d7160d94781d08a576fb52565
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-08 23:09:55 -0700

    added the prompt for the app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -13,6 +14,114 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "Enter name of the Manager: "
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "Enter ID of the the manager: "
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "Enter email of the manager: "
+        },
+        {
+            type: "input",
+            name: "managerOffice",
+            message: "Enter office number of the manager: "
+        },
+        {
+            type: "input",
+            name: "1stEngName",
+            message: "Enter name of the 1st engineer: "
+        },
+        {
+            type: "input",
+            name: "1stEngID",
+            message: "Enter ID of the 1st engineer: "
+        },
+        {
+            type: "input",
+            name: "1stEngEmail",
+            message: "Enter email of the 1st engineer: "
+        },
+        {
+            type: "input",
+            name: "1stEngGitHub",
+            message: "Enter GitHub Username of the 1st engineer: "
+        },
+        {
+            type: "input",
+            name: "2ndEngName",
+            message: "Enter name of the 2nd engineer: "
+        },
+        {
+            type: "input",
+            name: "2ndEngID",
+            message: "Enter ID of the 2nd engineer: "
+        },
+        {
+            type: "input",
+            name: "2ndEngEmail",
+            message: "Enter email of the 2nd engineer: "
+        },
+        {
+            type: "input",
+            name: "2ndEngGitHub",
+            message: "Enter GitHub Username of the 2nd engineer: "
+        },
+  
+        {
+            type: "input",
+            name: "3rdEngName",
+            message: "Enter name of the 3rd engineer: "
+        },
+        {
+            type: "input",
+            name: "3rdEngID",
+            message: "Enter ID of the 3rd engineer: "
+        },
+        {
+            type: "input",
+            name: "3rdEngEmail",
+            message: "Enter email of the 3rd engineer: "
+        },
+        {
+            type: "input",
+            name: "3rdEngGitHub",
+            message: "Enter GitHub Username of the 3rd engineer: "
+        },
+        
+  
+        {
+            type: "input",
+            name: "internName",
+            message: "Enter name of the Intern: "
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "Enter ID of the Intern: "
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "Enter email of the Intern:"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Enter the school the intern studied at: "
+        },
+       
+    ]);
+  }
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
